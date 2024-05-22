3bf28aff90dc3c0b3de2edb943f9d2f78413144d
Ryan Lin <ry18@Ryans-MacBook.local>
2024-05-22 11:38:27 -0700

    small edit to spotify embed

--- frontend/src/components/SpotifyEmbed.js
+++ frontend/src/components/SpotifyEmbed.js
@@ -3,7 +3,7 @@ function SpotifyEmbed() {
 
     return (
         <div>
-            <iframe title="spotifyembed1" id="GameSpotifyEmbed" src="https://open.spotify.com/embed/playlist/3ulPSpX0BUMOCDtgSmywpq?utm_source=generator"></iframe>
+            <iframe title="spotifyembed1" id="GameSpotifyEmbed" src="https://open.spotify.com/embed/playlist/3ulPSpX0BUMOCDtgSmywpq?utm_source=generator" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
         </div >
     )
 } export default SpotifyEmbed;
